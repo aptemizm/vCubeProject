2faf8f6dd1ee6b1b04498626e0b3e96c4aabd03c
RebelVa <158839734+RebelVa@users.noreply.github.com>
2024-04-17 17:03:35 +0300

    Доп5
    Меню сделано, но можно добавить стиль

---
+++ vCub3/Assets/Скрипты/Игровойменеджер.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Игровойменеджер : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Buntar2");
+    }
+}
