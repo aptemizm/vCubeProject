1c234cb6f308633f068ece8e632d1834d7591ff2
Matvey200999 <159509171+Matvey200999@users.noreply.github.com>
2024-04-23 21:42:39 +0500

    фонарик
    светит прикольно

---
+++ vCub3/Assets/Scripts/Фонарик.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Фонарик : MonoBehaviour
+{
+    [SerializeField] Light _flashlight;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            _flashlight.enabled = !_flashlight.enabled;
+        }
+    }
+}
