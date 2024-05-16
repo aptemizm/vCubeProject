af608fa7e5545ba6e369c24496aee33dca6e27ed
LUF-FY <75016792+LUF-FY@users.noreply.github.com>
2024-05-16 16:16:16 +0300

    Исправил кнопку

---
+++ vCub3/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonController : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private UnityEvent _actionOn;
+    [SerializeField] private UnityEvent _actionOff;
+
+    private int _numberOfAffectedObjects;
+    private bool _isOn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Cube"))
+        {
+            _numberOfAffectedObjects++;            
+        }
+
+        Debug.Log(_numberOfAffectedObjects);
+
+        if (!_isOn && _numberOfAffectedObjects > 0)
+        {
+            _actionOn?.Invoke();
+            _isOn = true;
+            _animator.SetBool("on", _isOn);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Cube"))
+        {
+            _numberOfAffectedObjects--;
+        }
+
+        Debug.Log(_numberOfAffectedObjects);
+
+        if (_isOn && _numberOfAffectedObjects == 0) 
+        {
+            _actionOff?.Invoke();
+            _isOn = false;
+            _animator.SetBool("on", _isOn);
+        }
+    }
+}
