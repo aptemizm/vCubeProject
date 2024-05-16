65d508e83f3183c05037e75ff080e4802f409d60
LUF-FY <75016792+LUF-FY@users.noreply.github.com>
2024-05-16 18:22:51 +0300

    Исправил гитхаб Убрал лишнее

---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    [SerializeField] private Light _light;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            _light.enabled = !_light.enabled;
+        }
+    }
+}
