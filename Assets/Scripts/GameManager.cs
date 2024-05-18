36dc7e60fd72d611060ff2c87793de22bdd26354
LUF-FY <75016792+LUF-FY@users.noreply.github.com>
2024-05-18 23:34:45 +0300

    исправил гейм менеджер

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,60 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager: MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject MenuPause;
+    private PlayerController playerController;
+    private FlashLight flashLight;
+
+    private bool GamePaesed;
+
+    void Start()
+    {
+        MenuPause.SetActive(false);
+        playerController = FindObjectOfType<PlayerController>();
+        flashLight = FindObjectOfType<FlashLight>();
+        GamePaesed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaesed) 
+            {
+                PauseOff();
+                GamePaesed = false;
+            }
+            else
+            {
+                PauseOn();
+                GamePaesed = true;
+            }
+        }
+    }
+
+    public void PauseOn()
+    {
+        MenuPause.SetActive(true);
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        playerController.enabled = false;
+        flashLight.enabled = false;
+    }
+
+    public void PauseOff()
+    {
+        MenuPause.SetActive(false);
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        playerController.enabled = true;
+        flashLight.enabled = true;
+    }
+
     public void Quit()
     {
         Application.Quit();
@@ -12,37 +64,9 @@ public class GameManager: MonoBehaviour
     {
         SceneManager.LoadScene("locationV2");
     }
-    public void InMenu()
-    {
-        SceneManager.LoadScene("Buntar");
-    }
-
-    [SerializeField]
-    GameObject pause;
-    void Start()
-    {
-        pause.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            pause.SetActive(true);
-            Time.timeScale = 0;
-        }
-    }
-
-    public void PauseOff()
-    {
-        pause.SetActive(false);
-        Time.timeScale = 1;
-    }
-
     public void Menu()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene("Buntar");
         Time.timeScale = 1;
-    }
+    }  
 }
