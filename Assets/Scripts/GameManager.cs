1b2fcc6b30c483d1dd95c968b9c074d9d086d857
RebelVa <158839734+RebelVa@users.noreply.github.com>
2024-05-18 23:07:19 +0300

    Работа

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,4 +16,33 @@ public class GameManager: MonoBehaviour
     {
         SceneManager.LoadScene("Buntar");
     }
+
+    [SerializeField]
+    GameObject pause;
+    void Start()
+    {
+        pause.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void PauseOff()
+    {
+        pause.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
 }
